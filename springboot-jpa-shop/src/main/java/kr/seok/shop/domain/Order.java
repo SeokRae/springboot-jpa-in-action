ff1af8ab90c966dd3f5c423dca1157c9145c7354
seokrae <kslbsh@gmail.com>
2020-12-30 00:49:35 +0900

    [JPA Shop] 주문 엔티티, 레포지토리, 서비스 개발 및 테스트

--- springboot-jpa-shop/src/main/java/kr/seok/shop/domain/Order.java
+++ springboot-jpa-shop/src/main/java/kr/seok/shop/domain/Order.java
@@ -47,4 +47,50 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    /*
+        주문 엔티티를 생성할 때 사용한다.
+        주문 회원, 배송정보, 주문상품의 정보 를 받아서 실제 주문 엔티티를 생성한다.
+    */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /*
+        주문 취소
+        주문 취소시 사용한다.
+        주문 상태를 취소로 변경하고 주문상품에 주문 취소를 알린다.
+        만약 이미 배송을 완료한 상품이면 주문을 취소하지 못하도록 예외를 발생시킨다.
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        /* 취소 상태로 변경 */
+        this.setStatus(OrderStatus.CANCEL);
+        /* 주문 취소 처리 */
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+    //==조회 로직==//
+    /**전체 주문 가격 조회*/
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
--- springboot-jpa-shop/src/main/java/kr/seok/shop/domain/OrderItem.java
+++ springboot-jpa-shop/src/main/java/kr/seok/shop/domain/OrderItem.java
@@ -21,5 +21,31 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order; //주문
-    private int orderPrice; //주문 가격 private int count; //주문 수량
+
+    private int orderPrice; //주문 가격
+    private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        /* 주문한 수량 만큼 재고를 줄인다. */
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        /* 취소한 주문 수량만큼 상품의 재고를 증가시킨다. */
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
